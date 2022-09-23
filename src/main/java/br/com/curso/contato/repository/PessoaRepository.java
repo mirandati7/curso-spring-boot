eefb0381a096197dac1ea28a1800ae9deaee59fa
alex-mr7 <alex@mr7ti.com.br>
2022-09-22 21:44:41 -0300

    projeto repository

--- src/main/java/br/com/curso/contato/repository/PessoaRepository.java
+++ src/main/java/br/com/curso/contato/repository/PessoaRepository.java
@@ -1,45 +1,11 @@
 package br.com.curso.contato.repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import br.com.curso.contato.dto.PessoaDTO;
+import br.com.curso.contato.model.Pessoa;
 
-public class PessoaRepository {
-
-    public List<PessoaDTO> getAll() {
-        List<PessoaDTO> listPessoa = new ArrayList<>();
-
-        PessoaDTO pessoa1 = new  PessoaDTO();
-        pessoa1.setId(1);
-        pessoa1.setNome("Alex Miranda");
-        pessoa1.setEmail("alexsistemass@gmail.com");
-        pessoa1.setTelefone("62966360547");
-        listPessoa.add(pessoa1);
-
-        PessoaDTO pessoa2 = new  PessoaDTO();
-        pessoa2.setId(2);
-        pessoa2.setNome("Marilainny");
-        pessoa2.setEmail("mary@gmail.com");
-        pessoa2.setTelefone("6212342679");
-        listPessoa.add(pessoa2);
-
-
-        PessoaDTO pessoa3 = new  PessoaDTO();
-        pessoa3.setId(3);
-        pessoa3.setNome("Julio Cesar");
-        pessoa3.setEmail("juliocezar@gmail.com");
-        pessoa3.setTelefone("45957887897");
-        listPessoa.add(pessoa3);
-
-        PessoaDTO pessoa4 = new  PessoaDTO();
-        pessoa4.setId(4);
-        pessoa4.setNome("Eribilton");
-        pessoa4.setEmail("rribilton@gmail.com");
-        pessoa4.setTelefone("11966360547");
-        listPessoa.add(pessoa4);
-
-        return listPessoa;
-    }
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
     
 }
--- src/main/java/br/com/curso/contato/service/PessoaService.java
+++ src/main/java/br/com/curso/contato/service/PessoaService.java
@@ -1,30 +1,31 @@
 package br.com.curso.contato.service;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.curso.contato.dto.PessoaDTO;
+import br.com.curso.contato.model.Pessoa;
 import br.com.curso.contato.repository.PessoaRepository;
 
 @Service
 public class PessoaService {
 
+    @Autowired
+    PessoaRepository pessoaRepository;
+
 
     public PessoaDTO consultar(Integer id) {
-        
-        PessoaRepository pessoaRepository = new PessoaRepository();
 
-        List<PessoaDTO> listPessoa = pessoaRepository.getAll();
+        // List<Pessoa> listPessoa = pessoaRepository.f();
 
-        PessoaDTO pessoaSelecionada = new PessoaDTO();
+        // PessoaDTO pessoaSelecionada = new PessoaDTO();
 
-        for (PessoaDTO pessoa : listPessoa) {
-            if (pessoa.getId().equals(id)){
-                pessoaSelecionada = pessoa;
-            }
-        }
-        return pessoaSelecionada;
+        // for (PessoaDTO pessoa : listPessoa) {
+        //     if (pessoa.getId().equals(id)){
+        //         pessoaSelecionada = pessoa;
+        //     }
+        // }
+        return null;
     }
 
     /**
@@ -32,16 +33,28 @@ public class PessoaService {
      * @param pessoaDTO
      * @return
      */
-    public PessoaDTO salvar(PessoaDTO pessoaDTO) {
+    public PessoaDTO salvar(PessoaDTO dto) {
         System.out.println("Acessando a classe Service");
-        if (pessoaDTO.getIdade() < 18) {
+        
+        if (dto.getIdade() < 18) {
             System.out.println("Não e possível cadastrar menor de idade.");
+            
+            Pessoa model = new Pessoa();
+            model.setId(dto.getId());
+            model.setEmail(dto.getEmail());
+            model.setNome(dto.getNome());
+            model.setTelefone(dto.getTelefone());
+            model.setIdade(dto.getIdade());
+
+
+            Pessoa pessoaRetornada = pessoaRepository.save(model);
+            System.out.println("ID .. :" + pessoaRetornada.getId());
         } else {
-            pessoaDTO.setIdade(pessoaDTO.getIdade() + 5);
+            dto.setIdade(dto.getIdade() + 5);
             System.out.println("Cadastro feito com sucesso.");
         }
         
-        return pessoaDTO;
+        return dto;
     }
 
 
